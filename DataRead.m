f71507acf840a6f8646a1ce11e1e763a6b734033
unknown <jashanxchopra@gmail.com>
2019-04-04 13:26:22 -0600

    Started DataRead.m, this file will automatically read data and output

---
+++ DataRead.m
@@ -0,0 +1,17 @@
+%% ASEN 2004 - Rocket Bottle Lab
+%{
+
+    Authors: Jashan Chopra (107689146)
+    Date Created: April 4th, 2019
+
+Script Purposes and goals:
+  1) Read static test stand data
+  2) edit data to appropriate ranges
+  3) automate the data curtailing process
+  4) output data to matrix for statistical usage
+
+%}
+
+
+% General Housekeeping
+clc; clear; close all;
