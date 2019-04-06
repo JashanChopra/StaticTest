3fa974f39fe588348e06d69594693decc09bc4a9
Jashan Chopra <jashanxchopra@gmail.com>
2019-04-06 15:36:18 -0600

    bug fix, surprisngly good results for no matrix manipulation

--- DataRead.m
+++ DataRead.m
@@ -16,9 +16,6 @@ Script Purposes and goals:
 clc; clear; close all;
 
 %% Import the test stand data, define initial conditions
-filenames = ["LA8am_test3","LA8am_test4"];  % Create a vector of all filenames
-numFiles = size(filenames);                 % Number of files
-
 g = 9.80665;        % gravitational constant
 mProp = 1;          % [kg] [1 L = 1000g = 1kg]
 mBottle=.15;        %[kg] % Mass of Empty Bottle
@@ -35,14 +32,19 @@ Tair=300;   %[K] % Initital Temp of Air
 mAir=((Pgage+Patm)*volAir)/(R*Tair);    % Mass of Air Initital
 mAirFinal=((Patm)*volAir)/(R*Tair);     % Mass of Air Final
 
+frequency = 1.652 * 1000;               % [Hz] Sampling Rate
+
+filenames = ["LA8am_test3","LA8am_test4"];  % Create a vector of all filenames
+numFiles = length(filenames);                 % Number of files
+
 %% Iteration of deltaV calculation
 deltaV = zeros(numFiles,1);     % preallocate deltaV
 for f = filenames               % loop iterates over all files
 
     % automated data trim
     data = fileLoad(f);                                                 % Load data
-    indicies = find(data1 <= 0);                                        % negative indices
-    data1(indicies) = [];                                               % remove negative indices
+    indicies = find(data <= 0);                                        % negative indices
+    data(indicies) = [];                                               % remove negative indices
 
     % calculations
     time = (1 / frequency) * linspace(0,length(data),length(data))';    % time vector
@@ -53,6 +55,6 @@ for f = filenames               % loop iterates over all files
     mInitial = mBottle + mProp + mAir;                                  % initial mass
     mFinal = mInitial - mProp - mAir + mAirFinal;                       % final mass
 
-    deltaV(f,:) = isp*g*log(mInitial / mFinal);                         % [m/s] % DeltaV
+    deltaV(find(filenames==f),:) = isp*g*log(mInitial / mFinal);        % [m/s] % DeltaV
 
 end
