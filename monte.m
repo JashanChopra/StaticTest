e187bfc2d8e897673511a5dc5052133a6f6143f6
Jashan Chopra <jashanxchopra@gmail.com>
2019-04-10 23:09:58 -0600

    updated 4 reference

--- monte.m
+++ monte.m
@@ -22,36 +22,40 @@ tic
 %% BASE FOR LOOP CONTROLS MONTE CARLO SIMULATION
 
 % Preallocate final matrix
-monteMat = zeros(numTrials,6);
+monteMat = zeros(numTrials,9);
 
 for i = 1:numTrials
 
     %% Identify Constants from each model
 
         % Given Constants
-        g=9.81;             %[m/s] % Gravity Constant
-        Cd=.8;              % Dischart Coefficient
-        rhoAtm=.961;        %[kg/m3] % Ambient Air Density
-        rhoWater=1000;      %[kg/m3] % Density of Water
-        volBottle=.002;     %[m^3] % Empty bottle Volume
-        Patm=83427;         %[Pa]   % Atmospheric Pressure
-        gamma=1.4;          % Ratio of Specific Heats for Air
-        Dthroat=.021;       %[m] % Diameter of Throat
-        Dbottle=.105;       %[m] % Diameter of Bottle
-        R=287;              % Universal Gas Constant Air
-        mBottle=.15;        %[kg] % Mass of Empty Bottle
-        CDrag=.5;           % Drag Coefficient
-        Pgage=345738;       %[Pa] % Intial Gauge Pressure of air in bottle
-        volWater=.001;      %[m^3] % Initial Volume of Water
-        Tair=300;           %[K] % Initital Temp of Air
-        v0=0;               % [m/s] Initial Velocity
-        theta=pi/4;         %[rad] % Initial Angle
-        ls=.5;              %[m] % Length of Test Stand
-        mProp = 1;          % [kg] [1 L = 1000g = 1kg]
+        g=9.80665;              %[m/s] % Gravity Constant
+        Cd=.8;                  % Discharge Coefficient
+        rhoAtm=.961;            %[kg/m3] % Ambient Air Density
+        rhoWater=1000;          %[kg/m3] % Density of Water
+        volBottle=.002;         %[m^3] % Empty bottle Volume
+        Patm=83427;             %[Pa]   % Atmospheric Pressure
+        gamma=1.4;              % Ratio of Specific Heats for Air
+        Dthroat=.021;           %[m] % Diameter of Throat
+        Dbottle=.105;           %[m] % Diameter of Bottle
+        R=287;                  % Universal Gas Constant Air
+        mBottle=117/1000;       %[kg] % Mass of Empty Bottle
+        CDrag=.3361;            % Drag Coefficient
+        Pgage=275790;           %[Pa] % Intial Gauge Pressure of air in bottle
+        volWater=0.000962;      %[m^3] % Initial Volume of Water
+        Tair=275.372;           %[K] % Initital Temp of Air
+        theta=pi/4;             %[rad] % Initial Angle
+        mProp = 962/1000;       % [kg] [1 L = 1000g = 1kg]
 
         x0=0;   %[m] % Initial Horizontal Distance
         z0=.25; %[m] % Initial Vertical Distance
         y0 = 0; %[m] % Initial Lateral Distance
+
+        % Monte Carlo Changed Variables
+        volWater = normrnd(volWater,.0001);         %[m^3] % Initial Volume of Water
+        mBottle=normrnd(mBottle,.01);               % [kg] % mass bottle
+        CDrag=normrnd(CDrag,.1);                    % Drag Coefficient
+        Tair=normrnd(Tair,10);                      % [K] % Initital Temp of Air
 
         % Calculated Constants
 
@@ -59,8 +63,6 @@ for i = 1:numTrials
         mAir=((Pgage+Patm)*volAir)/(R*Tair);    % Mass of Air Initital
         Athroat=pi*(Dthroat/2)^2;               % Area of Throat
         Abottle=pi*(Dbottle/2)^2;               % Area of Bottle
-        mAirFinal=((Patm)*volAir)/(R*Tair);     % Mass of Air Final
-        mWater = rhoWater * volWater;           % Initial mass of water
 
         vx = 0; % Initial Velocity x
         vy = 0; % Initial Velocity y
@@ -93,25 +95,31 @@ for i = 1:numTrials
 
     % Put current max iterator values in final matrix
     monteMat(i,1) = max(xpos);
-    monteMat(i,2) = min(ypos);
+    monteMat(i,2) = max(ypos);
     monteMat(i,3) = max(zpos);
 
     %% Rocket ISP Model Datas
     % Note: Commented and formatted code is present in 'rocketMain_Isp.m'
 
-    filename1 = 'LA8am_test3';
-    data1 = load(filename1);
-    data1 = data1(2215:3300,3); % Just the summed load data
-    indicies = find(data1 <= 0);
-    data1(indicies) = [];
-    force1 = data1*4.44822; % Summed force converted to newtons
-    frequency = 1.652 * 1000; % [Hz] Sampling Rate
-    time = (1 / frequency) * linspace(0,length(data1),length(data1)); % time vector
-    time = time'; % make it into a column vector
-    isp = trapz(time,force1);
+    f = 'LA8am_test3';
+    data = fileLoad(f);
+    negData = data < 0;                                                 % negative data values
+    low = 4*mean(data(negData));                                        % mean of negative values
+    data = data + abs(low);                                             % add that mean to shift values
+    indicies = data <= 0;                                         % negative indices
+    data(indicies) = [];                                                % remove negative indices
+    frequency = 1.652 * 1000;                                           % [Hz] Sampling Rate
+    time = (1 / frequency) * linspace(0,length(data),length(data))';    % time vector
+    fitobject = fit(time,data,'smoothingspline');                       % cubic interp
+    fx = abs(differentiate(fitobject, time));                           % calculate slope of various points
+    deletion = find(fx <= 600);                                         % deletion parameter
+    data(deletion) = []; time(deletion) = [];                           % remove values from data
+    time = time - time(1);                                              % reset time to 0
+    fitobject = fit(time,data,'cubicinterp');                           % refit data
+    isp = integrate(fitobject,time(end),time(1));
     isp = isp / (mProp*g);
-    mInitial = mBottle + mProp + mAir; % Initial Mass
-    mFinal = mInitial - mProp - mAir + mAirFinal; % Final Mass
+    mInitial = mBottle + mProp;         % Initial Mass
+    mFinal = 125 / 1000;                % Final Mass
     deltaV = isp*g*log(mInitial / mFinal); % [m/s] % DeltaV
     vx = deltaV*cos(theta); % Initial Velocity x
     vy = 0;                 % Initial Velocity y
@@ -126,8 +134,93 @@ for i = 1:numTrials
 
     % Put current max iterator values in final matrix
     monteMat(i,4) = max(ds(:,4));
-    monteMat(i,5) = min(ds(:,6));
+    monteMat(i,5) = max(ds(:,6));
     monteMat(i,6) = max(ds(:,5));
+
+    %% Given Constants
+        g=9.80665;              %[m/s] % Gravity Constant
+        Cd=.8;                  % Discharge Coefficient
+        rhoAtm=.961;            %[kg/m3] % Ambient Air Density
+        rhoWater=1000;          %[kg/m3] % Density of Water
+        volBottle=.002;         %[m^3] % Empty bottle Volume
+        Patm=83427;             %[Pa]   % Atmospheric Pressure
+        gamma=1.4;              % Ratio of Specific Heats for Air
+        Dthroat=.021;           %[m] % Diameter of Throat
+        Dbottle=.105;           %[m] % Diameter of Bottle
+        R=287;                  % Universal Gas Constant Air
+        mBottle=117/1000;       %[kg] % Mass of Empty Bottle
+        CDrag=.3361;            % Drag Coefficient
+        Pgage=275790;           %[Pa] % Intial Gauge Pressure of air in bottle
+        volWater=0.000962;      %[m^3] % Initial Volume of Water
+        Tair=275.372;           %[K] % Initital Temp of Air
+        theta=pi/4;             %[rad] % Initial Angle
+        mProp = 962/1000;       % [kg] [1 L = 1000g = 1kg]
+
+        x0=0;   %[m] % Initial Horizontal Distance
+        z0=.25; %[m] % Initial Vertical Distance
+        y0 = 0; %[m] % Initial Lateral Distance
+
+        % Monte Carlo Changed Variables
+        volWater = normrnd(volWater,.0001);         %[m^3] % Initial Volume of Water
+        mBottle=normrnd(mBottle,.01);               % [kg] % mass bottle
+        CDrag=normrnd(CDrag,.1);                    % Drag Coefficient
+        Tair=normrnd(Tair,10);                      % [K] % Initital Temp of Air
+
+        % Calculated Constants
+
+        volAir=volBottle-volWater;              % Volume of air in Bottle Initial
+        mAir=((Pgage+Patm)*volAir)/(R*Tair);    % Mass of Air Initital
+        Athroat=pi*(Dthroat/2)^2;               % Area of Throat
+        Abottle=pi*(Dbottle/2)^2;               % Area of Bottle
+
+        vx = 0; % Initial Velocity x
+        vy = 0; % Initial Velocity y
+        vz = 0; % Initial Velocity z
+
+    %% Direct Interpolation
+    f = 'LA8am_test3';
+    data = fileLoad(f);
+    negData = data < 0;                                                 % negative data values
+    low = mean(data(negData));                                        % mean of negative values
+    data = data + abs(low);                                             % add that mean to shift values
+    indicies = data <= 0;                                         % negative indices
+    data(indicies) = [];                                                % remove negative indices
+    frequency = 1.652 * 1000;                                           % [Hz] Sampling Rate
+    time = (1 / frequency) * linspace(0,length(data),length(data))';    % time vector
+    fitobject = fit(time,data,'smoothingspline');                       % cubic interp
+    fx = abs(differentiate(fitobject, time));                           % calculate slope of various points
+    deletion = find(fx <= 1200);                                         % deletion parameter
+    data(deletion) = []; time(deletion) = [];                           % remove values from data
+    time = time - time(1);                                              % reset time to 0
+    fitobject = fit(time,data,'cubicinterp');                           % refit data\
+    state1Vec = [volAir vx vz vy x0 z0 y0];
+    tspan = 0:.00001:.5;
+    optstage1 = odeset('Events',@opt1);
+    [t, ds] = ode45(@(t,s) state1funcDI(t,s,Cd,Athroat,gamma,...
+        rhoAtm,Patm,Pgage,volAir,Abottle,mBottle,mAir,CDrag,volBottle,...
+        rhoWater,vw,fitobject), tspan, state1Vec, optstage1);
+    m2 = mAir;
+    Pair = Pgage + Patm;
+    state2Vec = [m2;ds(end,2);ds(end,3);ds(end,4);ds(end,5);ds(end,6);ds(end,7)];
+    tspan2 = t(end):.000001:6;
+    optstage2 = odeset('Events',@opt2); % This function modifies ode45 to stop
+    [t2,ds2] = ode45(@(t2,s) state2funcDI(t2,s,mAir,gamma,Pair,volBottle,R,Patm,...
+        Athroat,Cd,Abottle,rhoAtm,volAir,CDrag,mBottle,vw,fitobject),tspan2, state2Vec,optstage2);
+
+    % Remove values where z has gone below the surface of the Earth
+    indices = ds2(:,6) < 0;
+    ds2(indices,:) = [];
+
+        % Concatenate two stages
+        xpos=[ds(:,5);ds2(:,5)];
+        zpos=[ds(:,6);ds2(:,6)];
+        ypos=[ds(:,7);ds2(:,7)];
+
+    % Put current max iterator values in final matrix
+    monteMat(i,7) = max(xpos);
+    monteMat(i,8) = max(ypos);
+    monteMat(i,9) = max(zpos);
+
 end
 toc
 end
--- monteMain.m
+++ monteMain.m
@@ -20,13 +20,25 @@ tic
 clc; clear; close all;
 
 %% Varying Parameters
-numTrials = 100;       % number of trials
-windMult = -2;        % Wind multiplier
+numTrials = 5;        % number of trials
+windMult = 1.5;           % Wind multiplier
 
 % Wind Values [Randomized - normal]
 vwx = windMult*abs(randn(numTrials,1));     % Normal distribution, only negative values
 vwy = windMult*abs(randn(numTrials,1));
 vw = [vwx,vwy,zeros(numTrials,1)];          % Randomized wind vector
+
+% Initial Volume Water [Randomized - normal]
+    % variation directly in monte.m
+
+% Initial mass of bottle [Randomized - normal]
+    % standard deviation is .01 kg, 10g -- Too much?
+
+% Coefficient of drag [Randomized - normal]
+    % normal value is .5, .01 is acceptable variation from dirt, etc?
+
+% Tempature of Air [Randomized - normal]
+    % normal value is 300K, 1 degree kelvin variation, maybe more?
 
 %% Call to monte carlo simulation function
 monteMat = monte(numTrials,vw);    % call monte.m
@@ -70,14 +82,43 @@ for p = prob            % Iterates for each error ellipse wanted
     hold on
 end
 
+%% Plot Error Ellipses [DI Data]
+prob = [.99,.95,.9];    % probability values at 99%, 95%, 90%
+for p = prob            % Iterates for each error ellipse wanted
+
+    s = -2 * log(1 - p);                        % calculate the Mahalanobis radius
+    sigma = cov(monteMat(:,7),monteMat(:,8));   % calculate the sigma covariance
+    [V,D] = eig(s * sigma);                     % calculate eiganvals of weighted covariance
+    theta = linspace(0,2*pi,360);               % theta linspace vector for ellipse
+
+    radii = (V * sqrt(D)) * [cos(theta(:))' ; sin(theta(:))'];  % ellipse functions
+    x = radii(1,:) + mean(monteMat(:,7));                       % x component
+    y = radii(2,:) + mean(monteMat(:,8));                       % y component
+
+    figure(1)
+    plot(x,y)   % plotting
+    hold on
+end
+
+%% Remove outliers then create final plotes
+index = monteMat > 100;             % distances above 100 are not expected
+monteMat(index) = 0;                % delete these values
+
 figure(1)
 plot(mean(monteMat(:,1)),mean(monteMat(:,2)),'*','MarkerSize',10);    % Thermo mean
 plot(mean(monteMat(:,4)),mean(monteMat(:,5)),'*','MarkerSize',10);    % Rocket mean
+plot(mean(monteMat(:,7)),mean(monteMat(:,8)),'*','MarkerSize',10);    % DI mean
 p3 = scatter(monteMat(:,1),monteMat(:,2));          % All thermo data
 p4 = scatter(monteMat(:,4),monteMat(:,5));          % All rocket data
-alpha(p3,.1); alpha(p4,.1);                         % set transparency
-legend('99% Thermo','95% Thermo','90% Thermo','99% Rocket','95% Rocket',...
-    '90% Rocket','Average Thermo','Average Rocket','All Data Thermo','All Data Rocket')
+p5 = scatter(monteMat(:,7),monteMat(:,8));          % All DI data
+alpha(p3,.1); alpha(p4,.1); % alpha(p5,.1);           % set transparency
+
+legend('99% T','95% T','90% T',...
+    '99% Isp','95% Isp','90% Isp',...
+    '99% DI','95% DI','90% DI',...
+    'Thermo Avg','Isp Avg','DI Avg',...
+    'Thermo Data','Isp Data','DI Data')
+
 title('Thermodynamic & Rocket Model Error Ellipses')
 xlabel('Downrange Distance [m]')
 ylabel('Crossrange Distance [m]')
